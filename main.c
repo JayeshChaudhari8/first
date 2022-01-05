060d2b51bc817ca89bfcf87725df134cbb84381d
Jayesh Chaudhari <jayesh.chaudhari@kpit.com>
2022-01-05 17:07:54 +0530

    remove head

--- main.c
+++ main.c
@@ -3,12 +3,10 @@
 
 int main()
 {
-<<<<<<< HEAD
+
     printf("Hello, welcome in KPIT...!\n")
     printf("Hello world..!\n");
     printf("Hello...!\n")
-=======
-    printf("Hello, welcome...!\n")
->>>>>>> alpha
+
     return 0;
 }
